dfba2686680402b8c16282c0f4f52d7727ba2ec3
chenjiandongx <chenjiandongx@qq.com>
2018-03-07 18:10:52 +0800

    更新 Header 组件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="header">
     <span id="title">📺 B 站视频各项数据排行榜 📆 更新至 2018-03-05</span>
-    <span id="profile">✨ 由 <a href="https://github.com/chenjiandongx">chenjiandongx</a> 贡献 ✨ </span>
+    <span id="profile">✨ 由 <a href="https://github.com/chenjiandongx">chenjiandongx</a> 贡献 ✨ <el-button type="text" @click="open">打赏作者</el-button> </span>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  export default {
+    methods: {
+      open() {
+        this.$alert('<div align="center"><img src="https://user-images.githubusercontent.com/19553554/35425854-504e716a-0299-11e8-81fc-4a511f1c47e8.png" width=231 height=265>', '谢谢您的慷慨捐赠！！', {
+          dangerouslyUseHTMLString: true
+        });
+      }
+    }
+  };
 </script>
 
 <style scoped>
@@ -16,7 +24,7 @@
   }
   #profile {
     float: right;
-    margin-right: 4%;
+    margin-right: 1%;
   }
   #title {
     margin-left: 1%;
